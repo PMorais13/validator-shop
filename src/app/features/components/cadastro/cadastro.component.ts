7e150f91063fe4de6eb76a344c3379dbaeca2d18
Vinycius Oliveira <vinyciusoliveira05@gmail.com>
2021-06-14 19:50:04 -0300

    refactor: CadastroComponent submit logics

--- src/app/features/components/cadastro/cadastro.component.ts
+++ src/app/features/components/cadastro/cadastro.component.ts
@@ -1,50 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-cadastro',
-  templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+    selector: 'app-cadastro',
+    templateUrl: './cadastro.component.html',
+    styleUrls: ['./cadastro.component.css']
 })
 
 export class CadastroComponent implements OnInit {
 
-  cadastro: FormGroup;
-  link = '';
+    public cadastro: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+    constructor(
+        private fb: FormBuilder,
+        private readonly router: Router,
+    ) {}
 
-  get f() {
-    return this.cadastro.controls;
-  }
-
-  ngOnInit() {
-
-    this.cadastro = this.fb.group({
-      empresa: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(256)]],
-      email: ['', [Validators.required, Validators.minLength(13), Validators.email]],
-      nome: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(256)]],
-      sobrenome: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(256)]],
-      endereco: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(256)]],
-      cidade: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(256)]],
-      estado: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]],
-      cep: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(8)]],
-
-    })
-  }
-
-  salvar() {
-    if (this.cadastro.invalid) {
-      return
-    } else {
-      return this.link = '/credit-card';
+    public get f() {
+        return this.cadastro.controls;
     }
 
+    public ngOnInit(): void {
 
-  }
+        this.cadastro = this.fb.group({
+            empresa: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(256)]],
+            email: ['', [Validators.required, Validators.minLength(13), Validators.email]],
+            nome: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(256)]],
+            sobrenome: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(256)]],
+            endereco: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(256)]],
+            cidade: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(256)]],
+            estado: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]],
+            cep: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(8)]],
+        })
+    }
 
-  reset(): void {
-    this.cadastro.reset();
-  }
+    /** Rotaciona para credit-card caso formulário seja válido. */
+    public salvar(): void {
+
+        if (this.cadastro.valid) {
+            this.router.navigate(['/credit-card']);
+        }
+    }
+
+    public reset(): void {
+        this.cadastro.reset();
+    }
 
 }
